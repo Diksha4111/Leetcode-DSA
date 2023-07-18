781d09191c659d11e1bbe089d2a674ee0e92f55a
Diksha Yadav <yadav.diksha1503@gmail.com>
2023-07-19 03:18:40 +0530

    Create Distance of nearest cell having 0.java

---
+++ Graphs/Distance of nearest cell having 0.java
@@ -0,0 +1,54 @@
+class Tuple{
+    int row;
+    int col;
+    int dist;
+    public Tuple(int row, int col , int dist){
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] vis = new int[n][m];
+        int[][] distance = new int[n][m];
+        Queue<Tuple> q = new LinkedList<Tuple>();
+        //finding 0's
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(mat[i][j] == 0){
+                    vis[i][j] = 1;
+                    q.offer(new Tuple(i ,j , 0));
+                }
+                else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        //calculating net difference of indices of row and col
+        int[] delrow = {-1 , 0 , 1 , 0};
+        int[] delcol = {0 , 1 , 0 , -1};
+
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            int dist = q.peek().dist;
+            q.poll();
+            distance[row][col] = dist;
+            //calculating distance of nearest 0 from each cell in all 4 directions
+            for(int i=0 ; i<4 ; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    q.offer(new Tuple(nrow , ncol , dist+1));
+                }
+            } 
+        }
+        return distance;
+    }
+}
